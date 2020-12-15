0a925bbc59908a8fe11bfe644fcb1356aa1bd3db
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-12-15 00:18:52 -0300

    add: algumas alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,90 @@
-import { Component } from '@angular/core';
+import { 
+  Component, 
+  AfterContentChecked, 
+  AfterContentInit,
+  AfterViewChecked, 
+  AfterViewInit ,
+  DoCheck, 
+  OnChanges, 
+  OnDestroy, 
+  OnInit, 
+  Input, 
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls:['app.component.css']
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'projeto-life-cycle';
+
+export class AppComponent implements OnChanges, OnInit, DoCheck,
+AfterContentInit, AfterContentChecked,AfterViewInit,
+AfterViewChecked,OnDestroy {
+  
+  displayChild: boolean=false;
   valor: number = 5;
-
   deletarCiclo: boolean = false;
+  showOff: boolean = false;
 
+  valorStr: string = ""
+
+
+  constructor() {
+    this.log("AppComponent:Constructor");
+  }
+  
+  toggle() {
+    this.displayChild=!this.displayChild;
+  }
+  
+  ngOnChanges() {
+    this.log("AppComponent:OnChanges");
+  }
+  ngOnInit() {
+    this.log("AppComponent:OnInit");
+  }
+  ngDoCheck() {
+    this.log("AppComponent:DoCheck");
+  }
+  ngAfterContentInit() {
+    this.log("AppComponent:AfterContentInit");
+  }
+  ngAfterContentChecked() {
+    this.log("AppComponent:AfterContentChecked");
+  }
+  ngAfterViewInit() {
+    this.log("AppComponent:AfterViewInit");
+  }
+  ngAfterViewChecked() {
+    this.log("AppComponent:AfterViewChecked");
+  }
+  ngOnDestroy() {
+    this.log("AppComponent:OnDestroy");
+  }
+  
+  private log(str: string) {
+    console.log(str);
+  }
+  
   mudarValor() {
     this.valor++;
   }
 
-  destruirCiclo(){
+  exibirTexto() {
+    if( this.showOff ) {
+      this.showOff = false;
+      this.valorStr = "Valor não exibido."
+    }
+    else { 
+      this.showOff = true;
+      this.valorStr = "Valor exibido."
+    }
+  }
+
+  destruirCiclo() {
+    
     this.deletarCiclo = true;
   }
+  
 }
